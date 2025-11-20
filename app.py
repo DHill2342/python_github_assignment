ac735c1501a59c0da1043eec633805ed1df5316f
Daniel Hill <hill279@usf.edu>
2025-11-20 00:19:02 -0500

    Add basic error handling for user input

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ sales = input("How much did we make in sales today? ")
 sales = float(sales)
 weekly_sales = sales * 7
 print("If we make that each day, we'll make $", weekly_sales, "this week.")
+try:
+    sales = float(sales)
+except ValueError:
+    print("Please enter a valid number.")
+    exit()
