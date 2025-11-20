60ce133b77be0048ad42487b4a51a0bac1f58a25
Daniel Hill <hill279@usf.edu>
2025-11-20 14:01:44 -0500

    Needed to rearrange parts so the except ValueError message would appear

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 print("Welcome to my Python program!")  #Welcome message
 sales = input("How much did we make in sales today? ") #User input for sales
-sales = float(sales) #Converts sales input to sales float
+try:
+    sales = float(sales) #Converts input to float (anticipated type)
+except ValueError:
+    print("Please enter a valid number.") #Error message for invalid input
+    exit() #Exits the program if input is invalid
 weekly_sales = sales * 7 #Calculates the weekly sales based on sales input
 print(f"If we make that each day, we'll make ${weekly_sales: .2f} this week.") #Prints weekly sales with supporting text
-try:
-    sales = float(sales)
-except ValueError:
-    print("Please enter a valid number.")
-    exit() #Exits the program if input is invalid
