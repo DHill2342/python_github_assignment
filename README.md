232ed38ef9a85a1de27dee9f6a2116f3c88e7ab9
Daniel Hill <hill279@usf.edu>
2025-11-20 00:35:50 -0500

    Add README with description, run instructions, and sample output

---
+++ README.md
@@ -0,0 +1,14 @@
+Weekly Sales Calculation
+
+This code will calculate your company's weekly sales based off of how much sales were made in one day. 
+To do this, input a valid number for the sales input, and then the code will multiply that number by 7 to calculate the weekly sales.
+
+python app.py
+# or
+python3 app.py
+
+Example Output:
+How much did we make in sales today? 700
+If we make that each day, we'll make $ 4900 this week.
+
+
--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-print("Welcome to my Python program!")
-sales = input("How much did we make in sales today? ")
-sales = float(sales)
-weekly_sales = sales * 7
-print("If we make that each day, we'll make $", weekly_sales, "this week.")
+print("Welcome to my Python program!")  #Welcome message
+sales = input("How much did we make in sales today? ") #User input for sales
+sales = float(sales) #Converts sales input to sales float
+weekly_sales = sales * 7 #Calculates the weekly sales based on sales input
+print("If we make that each day, we'll make $", weekly_sales, "this week.") #Prints weekly sales with supporting text
 try:
     sales = float(sales)
 except ValueError:
     print("Please enter a valid number.")
-    exit()
+    exit() #Exits the program if input is invalid
