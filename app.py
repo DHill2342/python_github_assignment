5164160d3c4796678ce6f823f33c48c53cd30060
Daniel Hill <hill279@usf.edu>
2025-11-20 00:06:24 -0500

    Add user input prompt

--- app.py
+++ app.py
@@ -1,4 +1,2 @@
 print("Welcome to my Python program!")
-hours = input("How many hours did you study today? ")
-print ('hello')
-print(f"You studied for {hours} hours today')")
+sales = input("How much did we make in sales today? ")
