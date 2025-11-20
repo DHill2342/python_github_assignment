745a1873d8ac123ed9738073a563606146c99884
Daniel Hill <hill279@usf.edu>
2025-11-20 00:17:38 -0500

    Add formatted output displaying calculation results

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ print("Welcome to my Python program!")
 sales = input("How much did we make in sales today? ")
 sales = float(sales)
 weekly_sales = sales * 7
+print("If we make that each day, we'll make $", weekly_sales, "this week.")
